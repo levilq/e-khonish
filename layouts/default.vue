59a4be5f20122f39d11dca42be49774cf58bdc29
levilq <amonbekov.loiq@gmail.com>
2025-07-14 09:45:59 +0500

    Blurry Circles

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,17 +4,27 @@
 
 <template>
   <div class="relative min-h-screen overflow-hidden" style="font-family: 'GTWalsheimPro', sans-serif;">
-    <!-- Blurry Circles -->
-    <div class="absolute bottom-[-120px] left-[-120px] w-[400px] h-[400px] bg-[#FFA41B]/70 rounded-full blur-[150px] z-0"></div>
-    <div class="absolute  right-[-100px] w-[400px] h-[400px] bg-[#00CFFF]/70 rounded-full blur-[150px] z-0"></div>
+    <!-- Blurry Circles in all corners -->
+    <div class="absolute top-[520px] left-[-120px] w-[500px] h-[450px] bg-[#FFA41B]/70 rounded-full blur-[150px] z-0"></div>
+    <div class="absolute top-[-120px] right-[-120px] w-[400px] h-[400px] bg-[#00CFFF]/70 rounded-full blur-[150px] z-0"></div>
+    <div class="absolute top-[1620px] right-[-120px] w-[400px] h-[400px] bg-[#FFA41B]/70 rounded-full blur-[150px] z-0"></div>
+    <div class="absolute top-[2420px] left-[-120px] w-[500px] h-[450px] bg-[#00CFFF]/70 rounded-full blur-[150px] z-0"></div>
+    <div class="absolute top-[3120px] right-[-120px] w-[400px] h-[400px] bg-[#00CFFF]/70 rounded-full blur-[150px] z-0"></div>
+    <div class="absolute top-[4000px] left-[-120px] w-[500px] h-[450px] bg-[#00CFFF]/70 rounded-full blur-[150px] z-0"></div>
+    <div class="absolute top-[4200px] right-[-120px] w-[400px] h-[400px] bg-[#FFA41B]/70 rounded-full blur-[150px] z-0"></div>
+
+    <div class="absolute bottom-[420px] left-[-120px] w-[400px] h-[400px] bg-[#FFA41B]/70 rounded-full blur-[150px] z-0"></div>
+    <div class="absolute bottom-[420px] right-[-120px] w-[400px] h-[400px] bg-[#00CFFF]/70 rounded-full blur-[150px] z-0"></div>
+
+    <!-- Optional center blur -->
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[300px] h-[300px] bg-[#FFA41B]/20 rounded-full blur-[120px] z-0"></div>
 
     <!-- Main container -->
     <div class="relative z-10 container mx-auto max-w-[1128px]">
       <OrganismsHeader class="mx-3 md:mx-0" />
       <slot />
-      <OrganismsFooterSection/>
+      <OrganismsFooterSection />
     </div>
-
   </div>
 </template>
 

eb38454eab6145480521ab1b016dcc8211dce442
ZprogT <68769821+ZprogT@users.noreply.github.com>
2025-12-16 01:03:31 +0700

    fix url

--- components/atoms/buttons/Primary.vue
+++ components/atoms/buttons/Primary.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-
+const baseURL = useRuntimeConfig().public.baseURL
 </script>
 
 <template>
-  <nuxt-link to="https://emaorif.com/authentication">
+  <nuxt-link :to="`${baseURL}/authentication`">
   <button class="capitalize bg-[#005B88] text-white px-5 py-2 font-semibold rounded-xl text-sm hover:bg-[#00466a] cursor-pointer">
     <slot/>
   </button>
